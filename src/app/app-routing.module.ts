7549cf66b715ccd142b7f8ec910ec5e7f730e6db
sumant kawathekar <suma.kawathekar@gmail.com>
2021-02-12 15:39:44 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,50 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { IntroComponent } from './intro/intro.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { ProfessionalInfoComponent } from './professional-info/professional-info.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: IntroComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'about',
+    component: AboutMeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'personal-info',
+    component: PersonalInfoComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'professional-info',
+    component: ProfessionalInfoComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'technologies',
+    component: TechnologiesComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { IntroComponent } from './intro/intro.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { ProfessionalInfoComponent } from './professional-info/professional-info.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { TechnologiesComponent } from './technologies/technologies.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    IntroComponent,
+    PersonalInfoComponent,
+    ProfessionalInfoComponent,
+    ProjectsComponent,
+    TechnologiesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
