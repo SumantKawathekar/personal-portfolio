dd916e3cae042f07d788ca18811a62886a9ae032
sumant kawathekar <suma.kawathekar@gmail.com>
2021-02-17 19:10:32 +0530

    Portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,30 +7,36 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { IntroComponent } from './intro/intro.component';
-import { PersonalInfoComponent } from './personal-info/personal-info.component';
 import { ProfessionalInfoComponent } from './professional-info/professional-info.component';
-import { ProjectsComponent } from './projects/projects.component';
 import { TechnologiesComponent } from './technologies/technologies.component';
 import { ContactComponent } from './contact/contact.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { EducationComponent } from './education/education.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutMeComponent,
     IntroComponent,
-    PersonalInfoComponent,
     ProfessionalInfoComponent,
-    ProjectsComponent,
     TechnologiesComponent,
-    ContactComponent
+    ContactComponent,
+    EducationComponent
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxSpinnerModule,
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { NgxSpinnerService } from 'ngx-spinner';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  public showSucces = false;
+  public submitted = false;
+  public contactForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly spinner: NgxSpinnerService
+    ) { }
 
   ngOnInit(): void {
+    this.initiateForm();
+  }
+
+  private initiateForm() {
+    this.contactForm = this.fb.group({
+      contactName: new FormControl('', Validators.required),
+      contactEmail: new FormControl('', Validators.required),
+      contactSubject: new FormControl(''),
+      contactMessage: new FormControl('', Validators.required)
+    })
+  }
+
+  public submitContactRequest() {
+   this.submitted = true;
+   this.spinner.show();
+ 
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.spinner.hide();
+    }, 5000);
+
+    this.showSucces = true;
+   
+
   }
 
 }
