4cb433a6d81e0d6d0c80d64b2646b43c98209aea
sumant kawathekar <suma.kawathekar@gmail.com>
2021-03-02 11:55:23 +0530

    Added download resume button on professional page Removed download button from personal page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AppService } from './app.service';
 import { IndexdbService } from './indexdb.service';
 
@@ -11,7 +12,8 @@ export class AppComponent implements OnInit {
   userObj: Promise<string>;
   constructor(
     private readonly indexdbService: IndexdbService,
-    private readonly appService: AppService){}
+    private readonly appService: AppService,
+    private readonly router: Router){}
   title = 'personal-portfolio';
   async ngOnInit() {
     await this.indexdbService.connectDB();
@@ -19,6 +21,9 @@ export class AppComponent implements OnInit {
     if(users) {
       this.userObj = JSON.parse(users);
       this.appService.setUserInfoObs(this.userObj);
+      this.router.navigate(['/intro'])
+    } else {
+      this.router.navigate(['/create'])
     }
    
   }
